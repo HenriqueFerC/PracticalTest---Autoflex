f0adbd2491e5838068684751d3fc13c7715213a8
HenriqueFerC <henriqueferreiracardoso179411@hotmail.com>
2026-02-05 16:37:36 -0300

    ProductDto validation implemented

--- src/main/java/com/example/demo/dto/ProductDto/UpdateProductDto.java
+++ src/main/java/com/example/demo/dto/ProductDto/UpdateProductDto.java
@@ -1,8 +1,18 @@
 package com.example.demo.dto.ProductDto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 import java.math.BigDecimal;
 
 public record UpdateProductDto(
+        @NotBlank(message = "Product Name cannot be blank!")
+        @Length(min = 2, max = 30, message = "Product Name must be between 2 and 30 letters long!")
         String name,
-        BigDecimal value) {
+        @NotNull(message = "Product Value cannot be null!")
+        @Min(value = 0, message = "Product Value cannot be negative!")
+        BigDecimal value
+) {
 }
