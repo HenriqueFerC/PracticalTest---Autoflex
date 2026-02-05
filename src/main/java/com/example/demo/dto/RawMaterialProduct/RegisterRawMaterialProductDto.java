06a573f9fa3ed3306bf633534e14cfabd74b801b
HenriqueFerC <henriqueferreiracardoso179411@hotmail.com>
2026-02-05 16:41:22 -0300

    RawMaterialProductDto validation implemented

--- src/main/java/com/example/demo/dto/RawMaterialProduct/RegisterRawMaterialProductDto.java
+++ src/main/java/com/example/demo/dto/RawMaterialProduct/RegisterRawMaterialProductDto.java
@@ -1,6 +1,11 @@
 package com.example.demo.dto.RawMaterialProduct;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 public record RegisterRawMaterialProductDto(
+        @NotNull(message = "Quantity Raw Material to One Product cannot be null!")
+        @Min(value = 1, message = "Quantity Raw Material to One Product cannot be less than 1!")
         Integer quantity
 ) {
 }
