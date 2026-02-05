160c789fd19edf4eb526f5d7ebf6a15052b434e2
HenriqueFerC <henriqueferreiracardoso179411@hotmail.com>
2026-02-05 16:39:12 -0300

    RawMaterialDto validation implemented

--- src/main/java/com/example/demo/dto/RawMaterialDto/RegisterRawMaterialDto.java
+++ src/main/java/com/example/demo/dto/RawMaterialDto/RegisterRawMaterialDto.java
@@ -1,8 +1,17 @@
 package com.example.demo.dto.RawMaterialDto;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 public record RegisterRawMaterialDto(
+        @NotBlank(message = "Raw Material Name cannot be blank!")
+        @Length(min = 2, max = 30, message = "Raw Material Name must be between 2 and 30 letters long!")
         String name,
+        @NotNull(message = "Raw Material stock cannot be null!")
+        @Min(value = 0, message = "Raw Material stock cannot be negative!")
         Integer stock
 ) {
 }
--- src/main/java/com/example/demo/dto/RawMaterialDto/UpdateRawMaterialDto.java
+++ src/main/java/com/example/demo/dto/RawMaterialDto/UpdateRawMaterialDto.java
@@ -1,7 +1,16 @@
 package com.example.demo.dto.RawMaterialDto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
 public record UpdateRawMaterialDto(
+        @NotBlank(message = "Raw Material Name cannot be blank!")
+        @Length(min = 2, max = 30, message = "Raw Material Name must be between 2 and 30 letters long!")
         String name,
+        @NotNull(message = "Raw Material stock cannot be null!")
+        @Min(value = 0, message = "Raw Material stock cannot be negative!")
         Integer stock
 ) {
 }
